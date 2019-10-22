07a4b325ce977ee10577e10d3c3de08afe40c179
manuel <manuelsturla@gmail.com>
2019-10-22 11:35:35 -0300

    Comienzo a encarar biblioteca dinamica
    Agrego Extern C, y empiezo a tratar de agregarla a la prueba del main

--- Bibliotecas/main.cpp
+++ Bibliotecas/main.cpp
@@ -1,15 +1,23 @@
 #include "BibliotecaEstatica/utilidades.h"
-#include "BibliotecaDinamica/PruebaMod.h"
+//#include "BibliotecaDinamica/PruebaMod.h"
 #include <vector>
 #include <iostream>
+#include <dlfcn.h>
 int main(){
+    //Prueba biblioteca estatica
 	std::vector<int> v = {1,2,3,4,5};
 	Sumador sum(v);
 	std::cout << "El sumador summo: "<<sum.sumar() << std::endl;
 
+	//Prueba bliblioteca dinamica
 	std::cout << "Inicio pruebas de libreria dinamica" << std::endl;
-	PruebaMod mod(v);
-	mod.activar();
+
+	void* handle = dlopen("/lib/libPruebaMod.so", RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+
 	std::cout << "El resultado del vector es: ";
 	for (auto num : v)
 		std::cout << num << ", ";
