292109184654d018a074572d929b9d8ec103e4b7
manuel <manuelsturla@gmail.com>
2019-10-27 16:26:34 -0300

    Hago el constructor del JugadorCPU y scriptLua

--- Lua/JugadorCPU.cpp
+++ Lua/JugadorCPU.cpp
@@ -2,7 +2,10 @@
 // Created by manfer on 26/10/19.
 //
 
+#include <algorithm>
 #include "JugadorCPU.h"
+#include "ParametrosLua/CadenaLua.h"
+#include "ParametrosLua/EnteroLua.h"
 
 #define FUNC_NUEVA_ACCION "obtenerNuevaAccion"
 #define FUNC_NUEVO_MAPA "crearMapa"
@@ -11,7 +14,16 @@ std::string JugadorCPU::obtenerAccion() {
     return ret[0]->obtenerValor().cadena;
 }
 
-JugadorCPU::JugadorCPU(const std::string& rutaScript) :
+JugadorCPU::JugadorCPU(const std::string& rutaScript, std::vector<std::string>& posPasto) :
     interprete(rutaScript){
-    interprete.ejecutarFuncion(FUNC_NUEVA_ACCION); //Faltan las cosas del mapa
+    //Creo una tabla con los datos del mapa,
+    // podría chequear que los strings estan separados con el SEPARADOR
+    std::string nombreTabla = "tablaAuxiliar";
+    for (size_t i = 1; i <= posPasto.size(); i++) {
+        CadenaLua posicion(posPasto[i-1]);
+        EnteroLua indice(i);
+        interprete.agregarElementoTabla(nombreTabla, indice, posicion);
+    }
+    std::vector<std::string> vars = {nombreTabla};
+    interprete.ejecutarFuncion(FUNC_NUEVO_MAPA, vars);
 }
--- Lua/JugadorCPU.h
+++ Lua/JugadorCPU.h
@@ -11,7 +11,7 @@
 class JugadorCPU {
     InterpreteLua interprete;
 public:
-    JugadorCPU(const std::string& rutaScript); //Puede recibir el mapa, para recibirlo una vez.
+    JugadorCPU(const std::string& rutaScript, std::vector<std::string>& posPasto); //Puede recibir el mapa, para recibirlo una vez.
     std::string obtenerAccion(); //recibe, posPropia, posAutosContrarios, posExtras.
 };
 
