e5c44be557d75e7ec92297c9e2ddacc9394bb233
Diego <diego.balestieri1998@gmail.com>
2019-11-07 14:56:36 -0300

    Agrego un detalle del menu

--- Client/Menu/Lobby.cpp
+++ Client/Menu/Lobby.cpp
@@ -15,11 +15,6 @@ Lobby::Lobby(ServidorProxy &servidor, QWidget* parent) : servidor(servidor) {
 
 void Lobby::conectar() {
     QObject::connect(findChild<QPushButton*>("crear"), &QPushButton::clicked, this, &Lobby::crearPartida);
-    QObject::connect(findChild<QPushButton*>("pista1"), &QPushButton::clicked, this, &Lobby::unirPartida);
-    QObject::connect(findChild<QPushButton*>("pista2"), &QPushButton::clicked, this, &Lobby::unirPartida);
-    QObject::connect(findChild<QPushButton*>("pista3"), &QPushButton::clicked, this, &Lobby::unirPartida);
-    QObject::connect(findChild<QPushButton*>("pista4"), &QPushButton::clicked, this, &Lobby::unirPartida);
-    QObject::connect(findChild<QPushButton*>("pista5"), &QPushButton::clicked, this, &Lobby::unirPartida);
 }
 
 void Lobby::crearPartida() {
@@ -29,20 +24,29 @@ void Lobby::crearPartida() {
 }
 
 void Lobby::copiarPistas(std::vector<std::string> &partidas) {
-    std::cout<<partidas.size()<<'\n';
     if(!partidas.empty()){
+        QObject::connect(findChild<QPushButton*>("pista1"), &QPushButton::clicked, this, &Lobby::unirPartida);
+        QObject::connect(findChild<QPushButton*>("pista1"), &QPushButton::clicked, this, &QWidget::close);
         findChild<QPushButton*>("pista1")->setText(QString::fromUtf8(partidas[0].c_str()));
     }
     if(partidas.size() > 1){
+        QObject::connect(findChild<QPushButton*>("pista2"), &QPushButton::clicked, this, &Lobby::unirPartida);
+        QObject::connect(findChild<QPushButton*>("pista2"), &QPushButton::clicked, this, &QWidget::close);
         findChild<QPushButton*>("pista2")->setText(QString::fromUtf8(partidas[1].c_str()));
     }
     if(partidas.size() > 2){
+        QObject::connect(findChild<QPushButton*>("pista3"), &QPushButton::clicked, this, &Lobby::unirPartida);
+        QObject::connect(findChild<QPushButton*>("pista3"), &QPushButton::clicked, this, &QWidget::close);
         findChild<QPushButton*>("pista3")->setText(QString::fromUtf8(partidas[2].c_str()));
     }
     if(partidas.size() > 3){
+        QObject::connect(findChild<QPushButton*>("pista4"), &QPushButton::clicked, this, &Lobby::unirPartida);
+        QObject::connect(findChild<QPushButton*>("pista4"), &QPushButton::clicked, this, &QWidget::close);
         findChild<QPushButton*>("pista4")->setText(QString::fromUtf8(partidas[3].c_str()));
     }
     if(partidas.size() > 4){
+        QObject::connect(findChild<QPushButton*>("pista5"), &QPushButton::clicked, this, &Lobby::unirPartida);
+        QObject::connect(findChild<QPushButton*>("pista5"), &QPushButton::clicked, this, &QWidget::close);
         findChild<QPushButton*>("pista5")->setText(QString::fromUtf8(partidas[4].c_str()));
     }
 }
