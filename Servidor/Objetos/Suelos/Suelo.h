7db715c5710ac12315763f24e75eb2e9e30034b4
Hugo <hlarrea@fi.uba.ar>
2019-11-18 21:46:27 -0300

    Barros se generan aleatoriamente

--- Servidor/Objetos/Suelos/Suelo.h
+++ Servidor/Objetos/Suelos/Suelo.h
@@ -2,6 +2,8 @@
 #define _SUELO_H_
 
 #include "../Interactuable.h"
+#include "../Modificadores/Modificador.h"
+#include <list>
 
 class Suelo: public Interactuable {
 protected:
@@ -10,6 +12,8 @@ public:
     virtual void interactuar(Carro *unCarro) = 0;
 
     virtual void revivirCarro(MundoBox2D &mundoBox2D, Carro *unCarro) = 0;
+
+    void colocarExtra(MundoBox2D &mundoBox2D, std::list<std::unique_ptr<Modificador>> *extras);
 };
 
 #endif
