d8a7c00861a7ede704b8830056816f250d0f13b9
Diego <diego.balestieri1998@gmail.com>
2019-11-25 14:32:07 -0300

    a

--- ClientConEventos/Vista/Visualizacion.cpp
+++ ClientConEventos/Vista/Visualizacion.cpp
@@ -7,8 +7,8 @@
 #include "../Excepciones/ExcepcionConPos.h"
 #include "../Sdl/Sonido.h"
 
-Visualizacion::Visualizacion(ServidorProxy &servidor, std::shared_ptr<Jugador> &jugador, int anchoPantalla,
-                             int alturaPantalla, int fpsRenderizacion, int aumentoCamara) : servidor(servidor),\
+Visualizacion::Visualizacion(ServidorProxy &servidor, std::shared_ptr<Jugador> &jugador, int anchoPantalla,\
+int alturaPantalla, int fpsRenderizacion, int aumentoCamara) : servidor(servidor),\
 renderizador("microMachines.exe", anchoPantalla, alturaPantalla, m, aumentoCamara),
 grabador(renderizador){
     this->fpsRenderizacion = fpsRenderizacion;
@@ -26,7 +26,6 @@ void Visualizacion::ejecutarPartida() {
     	std::chrono::high_resolution_clock::time_point t2;
     	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
     	int espera = 0;
-//    	grabador.grabarVideo("Videazo.mpeg");
         while(keepTalking && enJuego) {
             renderizador.limpiar();
             renderizador.copiarTodo();
@@ -36,6 +35,7 @@ void Visualizacion::ejecutarPartida() {
             t2 = std::chrono::high_resolution_clock::now();
             espera += (1000/fpsRenderizacion) - std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
             if(espera < 0){
+                std::cout<<"Tarde demasiado: "<<espera<<'\n';
                 t1 = t2;
             	continue;
             } else {
